4b35078b8c6be70bc61e6e28792ea7fc20c39e34
Icaro-Nunes <icarolimanunes@gmail.com>
2023-03-27 01:06:33 -0300

    Correção de bug em Aluno.clone() -> github não era clonado

--- ta-gui/src/app/aluno.ts
+++ ta-gui/src/app/aluno.ts
@@ -25,6 +25,7 @@ export class Aluno {
     aluno.nome = this.nome;
     aluno.cpf = this.cpf;
     aluno.email = this.email;
+    aluno.github = this.github;
     aluno.metas = this.cloneMetas();
     return aluno;
   }
